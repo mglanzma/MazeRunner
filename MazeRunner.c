49a1f9a12c865838e8f17779817e3be71f4893c3
Michael Glanzmann <mglanzma@mail.umw.edu>
2024-11-17 11:56:25 -0500

    Starting C file

---
+++ MazeRunner.c
@@ -0,0 +1,27 @@
+/*
+* MazeRunner
+*
+* Program by: Michael Glanzmann & Marshall Denson
+*
+* Class: CPSC 305 with Dr. Finlayson
+*
+*/
+
+// Screen dimensions
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 160
+
+
+/* NEED TO ADAPT CODE FOR OUR MAP/BACKGROUND AND SPRITE IMAGE
+*
+*
+//include the background image we are using 
+#include "background.h"
+
+//include the sprite image we are using
+#include "koopa.h"
+
+//include the tile map we are using
+#include "map.h"
+*/
+
