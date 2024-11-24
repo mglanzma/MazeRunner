abf06de27034a7f1f09cafb6be3b43677f9c56e5
Michael Glanzmann <mglanzma@mail.umw.edu>
2024-11-24 16:57:38 -0500

    Starting framework for assembly functions

---
+++ checkPlayerWin.s
@@ -0,0 +1,7 @@
+@ checkPlayerWin.s
+
+/* function to check if the player has won based on the win conditions */
+.global checkPlayerWin
+
+checkPlayerWin:
+//ToDo
---
+++ getFrameDigit.s
@@ -0,0 +1,7 @@
+@ getFrameDigit.s
+
+/* function to return the correct sprite frame based on the clock digit parameter */
+.global getFrameDigit
+
+getFrameDigit:
+//ToDo
