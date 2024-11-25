1cc772648eccc39b3f4cf2d46bf0a5fd064692e4
Marshall <130121577+marshallDenson@users.noreply.github.com>
2024-11-24 20:35:23 -0500

    First commit of checkPlayerWin assembly code

--- checkPlayerWin.s
+++ checkPlayerWin.s
@@ -4,4 +4,24 @@
 .global checkPlayerWin
 
 checkPlayerWin:
-//ToDo
+    cmp r0, #3
+    blt return_false
+    
+    cmp r1, #22 
+    beq return_true
+    cmp r1, #23 
+    beq return_true
+    cmp r1, #54 
+    beq return_true
+    cmp r1, #55 
+    beq return_true
+
+    b return_false
+
+return_true:
+    move r0, #1
+    mov pc, lr
+
+return_false:
+    mov r0, #0
+    mov pc, lr
